e026e5e50b389f0e1687516ceeb822dbfff15f9f
tuychievnegmat <tuychievnegmat@gmail.com>
2019-11-14 14:05:18 +0500

    есть строка, состоящая из слов. Все слова в ней разделены одним пробелом. Нужно преобразовать строку в такую структуру данных, которая группирует слова по первой букве в слове. Затем вывести только группы, содержащие болеее одного элемента. Группы должны быть отсортированны в алфавитном порядке. Слова внутри группы нужно сортировать по убыванию количества символов. Если количество символов равное, то сортировать в алфавитном порядке.
    задача исправлена с рекомендациями т.з.
    1. сортировка теперь сначала по убыванию, потом по алфавиту
    2. неиспользуемый импорт удален
    3. ошибка в коментарии исправлена
    4. возник вопрос, что имеется ввиду лишние строки между операциями.
    Но надеюсь исправил.
    5. Коллекция map sorted удалена и метод сразу возвращает нужный результат

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static java.util.Map.*;
 import static java.util.Map.Entry.comparingByKey;
 import static java.util.stream.Collectors.toMap;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "сапог сарай арбуз болт бокс биржа";
+        String s = "сапог сарай арбуз болт бокс биржа серебро фильм баржа барабан вода ветер вино ворон вокзал";
        Map<Character, List<String>> map = getGroupStr(s);
 
 
@@ -24,24 +23,34 @@ public class Main {
 
         List<String> strings = Arrays.asList(str.split(" "));
 
-
         Collections.sort(strings);
-        Function<String, Character> firstChar = s -> Character.toLowerCase(s.charAt(0));
 
+        Function<String, Character> firstChar = s -> Character.toLowerCase(s.charAt(0));
 
         Map<Character, List<String>> collect = strings.stream()
                 .collect(Collectors.groupingBy(firstChar));
 
-        //оортировка map по ключу
-         Map sorted = collect
+        for (Map.Entry<Character, List<String>> string: collect.entrySet()) {
+
+            Collections.sort(string.getValue(), new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    if(o1.length() > o2.length())return -1;
+                    else if(o1.length() < o2.length()) return 1;
+                    else return o1.compareTo(o2);
+                }
+            });
+        }
+
+        // cортировка map по ключу
+         return collect
                 .entrySet()
                 .stream()
                 .sorted(comparingByKey())
                 .collect(
                         toMap(Map.Entry::getKey, Map.Entry::getValue,
                                 (e1, e2) -> e2, LinkedHashMap::new));
-        return sorted;
-
+        
     }
 
 
